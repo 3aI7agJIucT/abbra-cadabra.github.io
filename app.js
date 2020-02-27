4527d10aa50a1732b58d3ac47948a35b7b1e0b9f
3aI7agJIucT <iphn4lvr@gmail.com>
2020-02-27 13:05:44 +0200

    initialized npm, added header

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+import React from 'react'
+
+import './../common/style/reset.css'
+import './../common/style/base.css'
+
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
+
+const App = () => {
+	return (
+		<div>
+			<Header/>
+		</div>
+	)
+}
+
+export default App
+
 $('.slider').slick({
 	dots: true,
 	infinite: true,
